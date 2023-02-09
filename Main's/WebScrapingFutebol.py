032377ff31b4bcbd7f6ef85c1db8e75996cdb080
José Henrique Siqueira <116378234+JoseHenriqueSiqueira@users.noreply.github.com>
2023-02-09 16:24:37 -0300

    Update

---
+++ Main's/WebScrapingFutebol.py
@@ -0,0 +1,112 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from subprocess import CREATE_NO_WINDOW
+from time import *
+import os
+
+class ChromeDriver():
+    def __init__(self):
+        self.SERVICE=None
+        wddir = os.environ["USERPROFILE"] + "\.wdm\drivers\chromedriver\win32" #Verificando se o usuário já tem o chrome web driver baixado
+        if os.path.exists(wddir):
+                wdname = "chromedriver.exe"
+                for root,dir,files in os.walk(wddir):
+                    if wdname in files:
+                        self.SERVICE=Service(os.path.join(root, wdname))
+        else:
+                self.SERVICE=Service(ChromeDriverManager().install()) #Baixando chrome web driver caso necessário
+        self.SERVICE.creationflags = CREATE_NO_WINDOW
+        options = Options()
+        options.headless = True
+        self.driver=webdriver.Chrome(service=self.SERVICE,options=options)
+
+class CopaDoBrasil2023(ChromeDriver):
+    def __init__(self) -> None:
+        super().__init__()
+        self.driver.set_window_size(1920, 1080)
+        self.driver.get("https://ge.globo.com/futebol/copa-do-brasil/")
+        self.etapas = {
+            "PrimeiraFase":0, "SegundaFase":1, "TerceiraFase":2, "Oitavas":3, "Quartas":4, "SemiFinal":5, "Final":6
+        }
+
+    def Etapa(self, etapa: str) -> list:
+        Dados=[]  
+        Botao = self.driver.find_element(By.CLASS_NAME, "navegacao-fase__seta-direita")
+        try:
+            for _ in range(self.etapas.get(etapa)):
+                Botao.click()
+        except:
+            return ["Parâmetro 'etapa' Inválido, verifique os parâmetros permitidos na documentação"]
+        Tabela = self.driver.find_elements(By.CLASS_NAME, "mata-mata__chave") 
+        for partida in Tabela:
+            Dados.append(partida.text.split('\n'))
+        return Dados
+
+class Brasileirao2022(ChromeDriver):
+    def __init__(self) -> None:
+        super().__init__()
+        self.driver.get('https://ge.globo.com/futebol/brasileirao-serie-a/')
+        self.driver.maximize_window()
+    
+    def TabelaClassificação(self) -> list:
+        Dados=[]
+        Posições = self.driver.find_elements(By.CLASS_NAME, "classificacao__equipes--posicao")
+        Nomes = self.driver.find_elements(By.CLASS_NAME, "classificacao__equipes--nome")
+        Pontos = self.driver.find_element(By.XPATH, "//*[@id='classificacao__wrapper']/article/section[1]/div/table[2]/tbody").text.split('\n')
+        for posicao, nome, resultados in zip(Posições, Nomes, Pontos):
+            Dados.append([posicao.text,nome.text,resultados])
+        return Dados
+
+    def Rodadas(self, rodada:int) -> list:
+        if type(rodada) != int or rodada <= 0 or rodada >= 39:
+            return ["Parâmetro 'rodada' Inválido, verifique os parâmetros permitidos na documentação"]
+        Dados=[]
+        Botao = self.driver.find_element(By.CLASS_NAME, "lista-jogos__navegacao--seta-esquerda svg")
+        for _ in range(38-(rodada)):
+            Botao.click()
+        UltimaRodada = self.driver.find_elements(By.CLASS_NAME, 'placar')
+        for Placar in UltimaRodada:
+            Dados.append(Placar.text.split('\n'))
+        return Dados
+
+class CopaDoMundo2022(ChromeDriver):
+    def __init__(self):
+        super().__init__()
+        self.driver.get('https://ge.globo.com/futebol/copa-do-mundo/2022/')
+        self.etapas = {
+            "Final":0, "TerceiroLugar":1, "SemiFinal":2, "Quartas":3, "Oitavas":4
+        }
+
+    def FaseDeGrupos(self) -> list:
+        Dados = []
+        Seta = self.driver.find_element(By.CLASS_NAME, "navegacao-fase__seta-esquerda")
+        for _ in range(6):
+            Seta.click()
+        Posiçoes = self.driver.find_elements(By.CLASS_NAME, "classificacao__equipes--posicao")
+        Nomes = self.driver.find_elements(By.CLASS_NAME, "classificacao__equipes--time")
+        Pontos = []
+        for n in range(1,9):
+            Pontos = Pontos + self.driver.find_element(By.XPATH, f"//*[@id='classificacao__wrapper']/article[{n}]/section[1]/div/table[2]/tbody").text.split("\n")
+        for posição, nome, pontos in zip(Posiçoes, Nomes, Pontos):
+            Dados.append([posição.text, nome.text, pontos])
+        return Dados
+
+    def Eliminatorias(self, etapa:str) -> list:
+        Dados = []
+        Seta = self.driver.find_element(By.CLASS_NAME, "navegacao-fase__seta-esquerda")
+        try:
+            for _ in range(self.etapas.get(etapa)):
+                Seta.click()
+        except:
+            return ["Parâmetro 'etapa' Inválido, verifique os parâmetros permitidos na documentação"]
+        Placares = self.driver.find_elements(By.CLASS_NAME, "placar")
+        for placar in Placares:
+            Dados.append(placar.text.split("\n"))
+        return Dados
+        
+
+if __name__ == "__main__":
+    pass
--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # Web_Scraping_2022/2023-Python
-<pre>
- Obtendo Informações do Campeonato Brasileiro 2022, Copa do Mundo 2022 e Topônimos/Gentílicos de Países através de Web Scraping com finalidade de aprendizado.
-</pre>
+Este repositório é dedicado a obtenção de informações de futebol através de Web Scraping, bem como à geração de bases de dados. As informações incluem:
+•Informações sobre a Copa Do Brasil 2023
+•Informações sobre gentílicos e topônimos de todos os países
+•Informações sobre as oitavas de final até a final da Copa do Mundo 2022
+•Informações sobre o Campeonato Brasileiro 2022
+![Update](https://img.shields.io/badge/update%20data-09%2F02%2F2023-brightgreen)
+
+Criado o script "WebScrapingFutebol.py", onde possui 4 classes. "CopaDoBrasil2023", "Brasileirão2022", "CopaDoMundo2022" e "ChromeDriver"
+    class CopaDoBrasil2023: Possui um método chamado "Etapa".
+        Etapa(etapa:str): Retorna os jogos, chave e placares de uma etapa específica passada como parâmetro pelo usuário
+            'etapa': Parâmetro do tipo STR, aceitando individualmente um dos seguintes valores:
+                {"PrimeiraFase", "SegundaFase", "TerceiraFase", "Oitavas", "Quartas", "SemiFinal" e "Final"}
+
+    class Brasileirão2022: Possui dois métodos, um chamado "TabelaClassificação" e "Rodadas".
+        TabelaClassificação(): Esse método retorna a tabela geral de classificação dos times.
+        Rodadas(rodada:int): Esse método retorna todos os placares da rodada passada como parâmetro pelo usuário.
+            'rodada': parâmetro do tipo INT, aceitando valores de '1' a '38'.
+
+    class CopaDoMundo2022: Possui dois métodos, um chamado "FaseDeGrupos" e "Eliminatorias".
+        FaseDeGrupos(): Esse método retorna a classificação geral e pontos gerais de todos os grupos da Copa Do Mundo 2022.
+        Eliminatorias(etapa:str): Retorna os jogos e placares de uma etapa específica passada como parâmetro pelo usuário
+            'etapa': Parâmetro do tipo STR, aceitando individualmente um dos seguintes valores:
+                {"Final", "TerceiroLugar", "SemiFinal", "Quartas", "Oitavas"}
+    
+    class ChromeDriver: Criada para optimizar o código e evitar que o usuário baixe o drive toda vez que for utilizar o script, assim deixando o script mais rapido e mais organizado.
+
+    obs: Todos os métodos retornam uma variável do tipo 'list'
+
+    Removido os arquivos "BrasileiraoMain.py" e "CopaDoMundo22Main.py", pois não são mais necessários no projeto.
+
+    Futuros Update: Atualizar os scripts exemplos, melhorar o Script "TopônimosGentílicosPaísesMain.py" (Possivelmente transformá-lo em uma Classe) e adicionar mais WebScraping.
+
+
 ![Update](https://img.shields.io/badge/update%20data-30%2F12%2F2022-brightgreen)
 <pre>
     Obtendo informações dos Gentílicos e Topônimos de todos os Países
